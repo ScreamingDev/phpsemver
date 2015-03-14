5d6a58f152ada637841ea7d94b0b4538daf7c3d6
Mike Pretzlaw <pretzlaw@gmail.com>
2015-03-14 03:45:14 +0100

    Check if a namespace has been added (github #19)

---
+++ lib/PHPSemVer/Rules/NamespaceRules/AddedRule.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 14.03.15
+ * Time: 03:25
+ */
+
+namespace PHPSemVer\Rules\NamespaceRules;
+
+
+use PHPSemVer\Rules\AbstractRule;
+use PHPSemVer\Rules\RuleInterface;
+
+class AddedRule extends AbstractRule implements RuleInterface
+{
+
+    public function process()
+    {
+        $prevNamespaces = array();
+        foreach ( $this->getPreviousBuilder()->getNamespaces() as $namespace )
+        {
+            $prevNamespaces[ ] = $namespace->getName();
+        }
+
+        foreach ( $this->getLatestBuilder()->getNamespaces() as $namespace )
+        {
+            if ( ! in_array( $namespace->getName(), $prevNamespaces ) )
+            {
+                $this->appendError(
+                    sprintf(
+                        'Found new class "%s".',
+                        $namespace->getName()
+                    )
+                );
+            }
+        }
+
+
+    }
+}
---
+++ lib/Test/PHPSemVer/Rules/NamespaceRules/AddedRuleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PHPSemVer\Rules\NamespaceRules;
+
+
+use PDepend\Source\Language\PHP\PHPBuilder;
+use Test\Abstract_TestCase;
+
+class AddedRuleTest extends Abstract_TestCase
+{
+    public function testItContainsErrorsWhenANamespaceIsMissing()
+    {
+        $mockBuilder = $this->getMockBuilder( get_class( new PHPBuilder() ) )
+                            ->setMethods( array( 'getNamespaces' ) );
+
+        $previousMock = $mockBuilder->getMock();
+        $previousMock->expects( $this->any() )
+                     ->method( 'getNamespaces' )
+                     ->willReturn(
+                         array()
+                     );
+
+        $latestMock = $mockBuilder->getMock();
+        $latestMock->expects( $this->any() )
+                   ->method( 'getNamespaces' )
+                   ->willReturn(
+                       array(
+                           new \PDepend\Source\AST\ASTNamespace( 'missing_one' )
+                       )
+                   );
+
+        $rule = new AddedRule( $previousMock, $latestMock );
+        $rule->process();
+
+        $this->assertNotEmpty( $rule->getErrors() );
+
+    }
+}
