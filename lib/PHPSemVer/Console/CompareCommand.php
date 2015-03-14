1caf53281087faf1fea1c858ba6483495d260e49
Mike Pretzlaw <pretzlaw@gmail.com>
2015-03-14 01:11:04 +0100

    Console: Semver2 is the default comparison (github #19)

--- lib/PHPSemVer/Console/CompareCommand.php
+++ lib/PHPSemVer/Console/CompareCommand.php
@@ -3,92 +3,125 @@
 namespace PHPSemVer\Console;
 
 use PDepend\Source\Language\PHP\PHPBuilder;
+use PHPSemVer\Rules\Semver2\Major\NamespaceRules\NoneDeletedRule;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CompareCommand extends AbstractCommand {
-	protected $_cacheFactory;
-	protected $cacheFactory;
-	/**
-	 * @var PHPBuilder
-	 */
-	protected $currentBuilder  = null;
-	protected $parseExceptions = array();
-	/**
-	 * @var PHPBuilder
-	 */
-	protected $previousBuilder = null;
+class CompareCommand extends AbstractCommand
+{
+    protected $_cacheFactory;
+    protected $cacheFactory;
+    /**
+     * @var PHPBuilder
+     */
+    protected $currentBuilder  = null;
+    protected $parseExceptions = array();
+    /**
+     * @var PHPBuilder
+     */
+    protected $previousBuilder = null;
 
-	protected function configure() {
-		$this->setName( 'compare' );
+    protected function configure()
+    {
+        $this->setName( 'compare' );
 
-		$this->addOption(
-			'type',
-			't',
-			InputArgument::OPTIONAL,
-			'Type of given targets',
-			'git'
-		);
+        $this->addOption(
+            'type',
+            't',
+            InputArgument::OPTIONAL,
+            'Type of given targets',
+            'git'
+        );
 
-		$this->addOption(
-			'ruleset',
-			'R',
-			InputArgument::OPTIONAL,
-			'A ruleset (eg. "semver2.0")',
-			'semver2.0'
-		);
+        $this->addOption(
+            'ruleset',
+            'R',
+            InputArgument::OPTIONAL,
+            'A ruleset (eg. "semver2.0")',
+            'semver2.0'
+        );
 
-		$this->addArgument(
-			'previous',
-			InputArgument::REQUIRED,
-			'Place to lookup the old code'
-		);
+        $this->addArgument(
+            'previous',
+            InputArgument::REQUIRED,
+            'Place to lookup the old code'
+        );
 
-		$this->addArgument(
-			'latest',
-			InputArgument::OPTIONAL,
-			'Place to lookup the new code',
-			'HEAD'
-		);
-	}
+        $this->addArgument(
+            'latest',
+            InputArgument::OPTIONAL,
+            'Place to lookup the new code',
+            'HEAD'
+        );
+    }
 
-	protected function execute(
-		InputInterface $input,
-		OutputInterface $output
-	) {
-		$this->verbose(
-			'Comparing %s "%s" with %s "%s" using "%s" ...',
-			$input->getOption( 'type' ),
-			$input->getArgument( 'latest' ),
-			$input->getOption( 'type' ),
-			$input->getArgument( 'previous' ),
-			$input->getOption( 'ruleset' )
-		);
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $this->verbose(
+            'Comparing %s "%s" with %s "%s" using "%s" ...',
+            $input->getOption( 'type' ),
+            $input->getArgument( 'latest' ),
+            $input->getOption( 'type' ),
+            $input->getArgument( 'previous' ),
+            $input->getOption( 'ruleset' )
+        );
 
-		$wrapper = $this->getWrapperClass( $input->getOption( 'type' ) );
+        $wrapper = $this->getWrapperClass( $input->getOption( 'type' ) );
 
-		if ( ! $wrapper ) {
-			$output->writeln(
-				sprintf(
-					'<error>Unknown wrapper-type "%s"</error>',
-					$input->getOption( 'type' )
-				)
-			);
+        if ( ! $wrapper )
+        {
+            $output->writeln(
+                sprintf(
+                    '<error>Unknown wrapper-type "%s"</error>',
+                    $input->getOption( 'type' )
+                )
+            );
 
-			return;
-		}
+            return;
+        }
 
-		$output->writeln( 'Done!' );
-	}
+        $previousWrapper = new $wrapper( $input->getArgument( 'previous' ) );
+        $latestWrapper   = new $wrapper( $input->getArgument( 'latest' ) );
 
-	public function getWrapperClass( $name ) {
-		$className = '\\PHPSemVer\\Wrapper\\' . ucfirst( $name );
+        $rule = new NoneDeletedRule( $previousWrapper->getBuilder(), $latestWrapper->getBuilder() );
+        $rule->process();
 
-		if ( ! class_exists( $className ) ) {
-			return false;
-		}
+        $table = new Table( $output );
+        $table->setHeaders(
+            array(
+                'Type',
+                'Message'
+            )
+        );
 
-		return $className;
-	}
+        foreach ( $rule->getErrors() as $error )
+        {
+            $table->addRow(
+                array(
+                    $error->getRule(),
+                    $error->getMessage()
+                )
+            );
+        }
+
+        $table->render();
+
+        $output->writeln( 'Done!' );
+    }
+
+    public function getWrapperClass( $name )
+    {
+        $className = '\\PHPSemVer\\Wrapper\\' . ucfirst( $name );
+
+        if ( ! class_exists( $className ) )
+        {
+            return false;
+        }
+
+        return $className;
+    }
 }
--- lib/PHPSemVer/Wrapper/Git.php
+++ lib/PHPSemVer/Wrapper/Git.php
@@ -5,107 +5,120 @@ namespace PHPSemVer\Wrapper;
 use GitWrapper\GitException;
 use GitWrapper\GitWrapper;
 
-class Git extends AbstractWrapper {
+class Git extends AbstractWrapper
+{
 
-	protected $_fileWrapper;
+    protected $_fileWrapper;
+    protected $_gitWrapper;
+    protected $_tempPath;
 
-	protected $_tempPath;
+    public function __construct( $base )
+    {
+        parent::__construct( $base );
 
-	public function __construct( $base ) {
-		parent::__construct( $base );
+        if ( ! is_dir( $this->getTempPath() ) )
+        {
+            mkdir( $this->getTempPath(), 0777, true );
+        }
 
-		if ( ! is_dir( $this->getTempPath() ) ) {
-			mkdir( $this->getTempPath(), 0777, true );
-		}
+        $this->_fileWrapper = new Directory( $this->getTempPath() );
+    }
 
-		$this->_fileWrapper = new Directory( $this->getTempPath() );
-	}
+    public function getTempPath()
+    {
+        if ( ! $this->_tempPath )
+        {
+            $this->_tempPath = sys_get_temp_dir()
+                               . DIRECTORY_SEPARATOR . uniqid( PHPSEMVER_ID );
+        }
 
-	public function getTempPath() {
-		if ( ! $this->_tempPath ) {
-			$this->_tempPath = sys_get_temp_dir()
-			                   . DIRECTORY_SEPARATOR . uniqid( PHPSEMVER_ID );
-		}
+        return $this->_tempPath;
+    }
 
-		return $this->_tempPath;
-	}
+    public function getAllFileNames()
+    {
 
-	protected $_gitWrapper;
+        $options = array(
+            'with-tree' => $this->getBase(),
+        );
 
-	/**
-	 * @return GitWrapper
-	 */
-	protected function _getGitWrapper() {
-		if ( ! $this->_gitWrapper ) {
-			$this->_gitWrapper = new GitWrapper();
-		}
+        $git = $this->_getGitWrapper()->workingCopy( getcwd() );
 
-		return $this->_gitWrapper;
-	}
+        $result = $git->run(
+            array(
+                'ls-files',
+                $options
+            )
+        );
 
-	public function getAllFileNames() {
+        $allPrevious = explode( PHP_EOL, $result->getOutput() );
 
-		$options = array(
-			'with-tree' => $this->getBase(),
-		);
+        return array_filter( $allPrevious );
+    }
 
-		$git = $this->_getGitWrapper()->workingCopy( getcwd() );
+    /**
+     * @return GitWrapper
+     */
+    protected function _getGitWrapper()
+    {
+        if ( ! $this->_gitWrapper )
+        {
+            $this->_gitWrapper = new GitWrapper();
+        }
 
-		$result = $git->run(
-			array(
-				'ls-files',
-				$options
-			)
-		);
+        return $this->_gitWrapper;
+    }
 
-		$allPrevious = explode( PHP_EOL, $result->getOutput() );
+    public function getBasePath()
+    {
+        return $this->_getFileWrapper()->getBasePath();
+    }
 
-		return array_filter( $allPrevious );
-	}
+    public function getPath( $fileName )
+    {
+        $fullName = $this->_getFileWrapper()->getPath( $fileName );
 
-	public function getPath( $fileName ) {
-		$fullName = $this->getBasePath() . $fileName;
+        if ( ! file_exists( $fullName ) )
+        {
+            $dir = dirname( $fullName );
+            if ( ! is_dir( $dir ) )
+            {
+                mkdir( $dir, 0777, true );
+            }
 
-		if ( ! file_exists( $fullName ) ) {
-			$dir = dirname( $fullName );
-			if ( ! is_dir( $dir ) ) {
-				mkdir( $dir, 0777, true );
-			}
+            file_put_contents( $fullName, '' );
 
-			file_put_contents( $fullName, '' );
+            // last state but suppress error messages
+            $gitWrapper = $this->_getGitWrapper();
+            $git        = $gitWrapper->workingCopy( getcwd() );
 
-			// last state but suppress error messages
-			$gitWrapper = $this->_getGitWrapper();
-			$git        = $gitWrapper->workingCopy( getcwd() );
-
-			try {
-				$git->run(
-					array(
-						'show',
-						$this->getBase() . '^:' . $fileName
-					)
-				);
+            try
+            {
+                $git->run(
+                    array(
+                        'show',
+                        $this->getBase() . '^:' . $fileName
+                    )
+                );
 
 
-				$content = $git->getOutput();
-			} catch ( GitException $e ) {
-				$content = '';
-			}
+                $content = $git->getOutput();
+            } catch ( GitException $e )
+            {
+                $content = '';
+            }
 
-			file_put_contents( $fullName, $content );
-		}
+            file_put_contents( $fullName, $content );
+        }
 
-		return $fullName;
-	}
+        return $fullName;
+    }
 
-	public function getBasePath() {
-		return $this->_getFileWrapper()->getBasePath();
-	}
-
-	/**
-	 * @return Directory
-	 */
-	protected function _getFileWrapper() {
-		return $this->_fileWrapper;
-	}
+    /**
+     * @return Directory
+     */
+    protected function _getFileWrapper()
+    {
+        return $this->_fileWrapper;
+    }
 }
