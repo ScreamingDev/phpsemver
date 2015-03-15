dbf87558de15a9a15e33a146009ab067ec6b1676
Mike Pretzlaw <pretzlaw@gmail.com>
2015-03-16 00:10:15 +0100

    \PHPSemVer\Specification can contain all RuleSets from XML (github #22)

--- lib/Test/PHPSemVer/SpecificationTest.php
+++ lib/Test/PHPSemVer/SpecificationTest.php
@@ -7,6 +7,24 @@ use PHPSemVer\Specification;
 
 class SpecificationTest extends \PHPUnit_Framework_TestCase
 {
+    public function testItAddsConfigOfXmlFiles()
+    {
+        $xml = PHPSEMVER_LIB_PATH
+               . DIRECTORY_SEPARATOR . 'PHPSemVer'
+               . DIRECTORY_SEPARATOR . 'Rules'
+               . DIRECTORY_SEPARATOR . 'SemVer2.xml';
+
+        $spec = new Specification();
+
+        $spec->updateFromXmlString( file_get_contents( $xml ) );
+
+        $ruleSets = $spec->getRuleSets();
+
+        $this->assertCount( 3, $spec->getRuleSets() );
+        $this->assertEquals( 'major', $ruleSets[ 0 ]->getName() );
+        $this->assertEquals( 'patch', $ruleSets[ 2 ]->getName() );
+    }
+
     public function testXmlCanAddRuleSets()
     {
         $spec = new Specification();
@@ -28,6 +46,14 @@ class SpecificationTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @return \SimpleXMLElement
+     */
+    public function getEmptyXml()
+    {
+        return simplexml_load_string( '<?xml version="1.0" ?> <phpsemver></phpsemver>' );
+    }
+
+    /**
      * @expectedException \Exception
      */
     public function testXmlThrowsErrorWhenRuleSetNameIsInvalid()
@@ -38,13 +64,5 @@ class SpecificationTest extends \PHPUnit_Framework_TestCase
         $xml->addChild( 'ruleSet' );
 
         $spec->updateFromXmlString( $xml->asXML() );
-    }
-
-    /**
-     * @return \SimpleXMLElement
-     */
-    public function getEmptyXml()
-    {
-        return simplexml_load_string( '<?xml version="1.0" ?> <phpsemver></phpsemver>' );
     }
 }
