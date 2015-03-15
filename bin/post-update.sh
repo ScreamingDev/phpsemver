6aa1bd022295a6e2e5332c2932d582c0d5cffaa8
Mike Pretzlaw <pretzlaw@gmail.com>
2015-03-15 10:14:57 +0100

    Hooks will be removed if they are no symlink

--- bin/post-update.sh
+++ bin/post-update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[[ ! -L .git/hooks/post-update ]] || rm .git/hooks/post-update
+[[ ! -L .git/hooks/pre-commit ]] || rm .git/hooks/pre-commit
+[[ ! -L .git/hooks/post-commit ]] || rm .git/hooks/post-commit
 
 [[ -f .git/hooks/post-update ]] || ln -s ../../bin/post-update.sh .git/hooks/post-update
 [[ -f .git/hooks/pre-commit ]] || ln -s ../../bin/pre-commit.sh .git/hooks/pre-commit
