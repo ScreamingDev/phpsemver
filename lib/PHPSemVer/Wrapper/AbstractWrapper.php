6fdf3d5cce29e6e8b42d60ae224a6605fcea5518
Mike Pretzlaw <pretzlaw@gmail.com>
2015-11-29 02:00:11 +0100

    Crap can be excluded via option and simple strings

--- lib/PHPSemVer/Wrapper/AbstractWrapper.php
+++ lib/PHPSemVer/Wrapper/AbstractWrapper.php
@@ -18,12 +18,14 @@ abstract class AbstractWrapper
     protected $_base;
     protected $_cacheFactory;
     protected $_parserExceptions;
+    protected $excludePattern;
 
-    public function __construct( $base )
+    public function __construct($base)
     {
-        if ( ! $base )
-        {
-            throw new \InvalidArgumentException( 'Please provide a base. Can not be empty.' );
+        if ( ! $base) {
+            throw new \InvalidArgumentException(
+                'Please provide a base. Can not be empty.'
+            );
         }
         $this->_base = $base;
     }
@@ -32,9 +34,8 @@ abstract class AbstractWrapper
     {
         $allPaths = array();
 
-        foreach ( $this->getAllFileNames() as $fileName )
-        {
-            $allPaths[ $fileName ] = $this->getBasePath() . $fileName;
+        foreach ($this->getAllFileNames() as $fileName) {
+            $allPaths[$fileName] = $this->getBasePath().$fileName;
         }
 
         return $allPaths;
@@ -61,10 +62,8 @@ abstract class AbstractWrapper
         $translator = new NikicParser();
         $dataTree   = new DataNode();
 
-        foreach ( $this->getAllFileNames() as $sourceFile )
-        {
-            if ( ! preg_match( '/\.php$/i', $sourceFile ) )
-            {
+        foreach ($this->getAllFileNames() as $sourceFile) {
+            if ( ! preg_match('/\.php$/i', $sourceFile)) {
                 continue;
             }
 
@@ -79,29 +78,6 @@ abstract class AbstractWrapper
         return $dataTree;
     }
 
-    protected function _getCache( $key = null )
-    {
-        return $this->_getCacheFactory()->create( $key );
-    }
-
-    /**
-     * @return CacheFactory
-     */
-    protected function _getCacheFactory()
-    {
-        if ( ! $this->_cacheFactory )
-        {
-            $settings                = new \stdClass();
-            $settings->cache         = new \stdClass();
-            $settings->cache->driver = 'memory';
-            $config                  = new Configuration( $settings );
-
-            $this->_cacheFactory = new CacheFactory( $config );
-        }
-
-        return $this->_cacheFactory;
-    }
-
     /**
      * @param $tokenizer
      * @param $builder
@@ -109,9 +85,9 @@ abstract class AbstractWrapper
      *
      * @return PHPParserGeneric
      */
-    public function getParser( $tokenizer, $builder, $cache )
+    public function getParser($tokenizer, $builder, $cache)
     {
-        return new PHPParserGeneric( $tokenizer, $builder, $cache );
+        return new PHPParserGeneric($tokenizer, $builder, $cache);
     }
 
     /**
@@ -120,5 +96,37 @@ abstract class AbstractWrapper
     public function getParserExceptions()
     {
         return $this->_parserExceptions;
+    }
+
+    public function setExcludePattern($pattern)
+    {
+        $this->excludePattern = $pattern;
+    }
+
+    public function getExcludePattern()
+    {
+        return (array) $this->excludePattern;
+    }
+
+    protected function _getCache($key = null)
+    {
+        return $this->_getCacheFactory()->create($key);
+    }
+
+    /**
+     * @return CacheFactory
+     */
+    protected function _getCacheFactory()
+    {
+        if ( ! $this->_cacheFactory) {
+            $settings                = new \stdClass();
+            $settings->cache         = new \stdClass();
+            $settings->cache->driver = 'memory';
+            $config                  = new Configuration($settings);
+
+            $this->_cacheFactory = new CacheFactory($config);
+        }
+
+        return $this->_cacheFactory;
     }
 }
