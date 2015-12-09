19b8c23288209af9a132a33416f228f35e29f508
Mike Pretzlaw <pretzlaw@gmail.com>
2015-12-09 07:41:27 +0100

    Added license and docs

--- lib/PHPSemVer/AbstractConfig.php
+++ lib/PHPSemVer/AbstractConfig.php
@@ -1,12 +1,29 @@
 <?php
+/**
+ * Contains config template.
+ *
+ * LICENSE: This source file is subject to the MIT license
+ * that is available through the world-wide-web at the following URI:
+ * https://opensource.org/licenses/MIT. If you did not receive a copy
+ * of the PHP License and are unable to obtain it through the web, please send
+ * a note to pretzlaw@gmail.com so we can mail you a copy immediately.
+ *
+ * @author    Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright 2015 Mike Pretzlaw
+ * @license   https://github.com/sourcerer-mike/phpsemver/tree/3.0.0/LICENSE.md MIT License
+ * @link      https://github.com/sourcerer-mike/phpsemver/
+ */
+
 
 namespace PHPSemVer;
 
 /**
- * Class Config
+ * Abstract config.
  *
- * @package PHPSemVer
- *
+ * @author    Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright 2015 Mike Pretzlaw
+ * @license   https://github.com/sourcerer-mike/phpsemver/tree/3.0.0/LICENSE.md MIT License
+ * @link      https://github.com/sourcerer-mike/phpsemver/
  */
 class AbstractConfig
 {
@@ -64,6 +81,8 @@ class AbstractConfig
     }
 
     /**
+     * Receive the wrapped SimpleXMLElement.
+     *
      * @return \SimpleXMLElement
      */
     public function getXml()
