d34d7ce3bb6c30a70f4a32fdac34ece22922a9e3
Mike Pretzlaw <pretzlaw@gmail.com>
2015-12-16 07:30:24 +0100

    Compare: Time is measured
    The output prints how long it took
    to gather the files,
    to parse the files
    to compare the states
    and to complete the whole compare command.

--- lib/Test/PHPSemVer/Console/CompareCommandTest.php
+++ lib/Test/PHPSemVer/Console/CompareCommandTest.php
@@ -55,7 +55,7 @@ class CompareCommandTest extends \PHPUnit_Framework_TestCase
         );
 
         $this->assertContains(
-            'Done',
+            'Total time',
             $commandTester->getDisplay()
         );
     }
