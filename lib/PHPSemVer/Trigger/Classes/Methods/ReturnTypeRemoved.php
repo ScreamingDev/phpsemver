4b582b03de07496fe8617280db65277ac689234f
Mike Pretzlaw <pretzlaw@gmail.com>
2016-01-02 04:16:33 +0100

    Trigger to test if return type of methods changed (#18)

---
+++ lib/PHPSemVer/Trigger/Classes/Methods/ReturnTypeRemoved.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Contains trigger.
+ *
+ * LICENSE: This source file is subject to the MIT license
+ * that is available through the world-wide-web at the following URI:
+ * https://opensource.org/licenses/MIT. If you did not receive a copy
+ * of the PHP License and are unable to obtain it through the web, please send
+ * a note to pretzlaw@gmail.com so we can mail you a copy immediately.
+ *
+ * @author    Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright 2016 Mike Pretzlaw
+ * @license   http://github.com/sourcerer-mike/phpsemver/LICENSE.md MIT License
+ * @link      http://github.com/sourcerer-mike/phpsemver
+ */
+
+namespace PHPSemVer\Trigger\Classes\Methods;
+
+
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPSemVer\Constraints\FailedConstraint;
+use PHPSemVer\Trigger\AbstractTrigger;
+
+/**
+ * Check if return type were removed.
+ *
+ * @author    Mike Pretzlaw <pretzlaw@gmail.com>
+ * @copyright 2016 Mike Pretzlaw
+ * @license   http://github.com/sourcerer-mike/phpsemver/LICENSE.md MIT License
+ * @link      http://github.com/sourcerer-mike/phpsemver
+ */
+class ReturnTypeRemoved extends AbstractTrigger
+{
+
+    /**
+     * Check if return type changed.
+     *
+     * @param ClassMethod $old
+     * @param ClassMethod $new
+     *
+     * @return bool|null
+     */
+    public function handle($old, $new)
+    {
+        if ( ! $this->canHandle($old) || ! $this->canHandle($new)) {
+            return null;
+        }
+
+        if ((string) $old->getReturnType() == (string) $new->getReturnType()) {
+            return false;
+        }
+
+        $this->lastException = new FailedConstraint(
+            sprintf(
+                '%s::%s() changed return type from "%s" to "%s".',
+                $new->getAttribute('parent')->namespacedName,
+                $new->name,
+                $old->getReturnType(),
+                $new->getReturnType()
+            )
+        );
+
+        return true;
+    }
+
+    public function canHandle($subject)
+    {
+        return ( $subject instanceof ClassMethod );
+    }
+}
